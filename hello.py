26ce6b1088e8f9cca0ef05355b23342de2cf1839
Efe-giltech <efepraise123@gmail.com>
2024-07-18 17:32:16 +0100

    Add files via upload
    concatenation refers to the operation of combining strings or other data sets. In python, this is achieved using the "+" operator

---
+++ hello.py
@@ -0,0 +1,5 @@
+firstName = input("input users firstName:")
+lastName = input("input users lastName:")
+state = input("input users state:")
+year = input("input users year:")
+print(firstName +" "+ lastName +" "+ state +" "+ year)
